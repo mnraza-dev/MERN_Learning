8d77b47daf5e7cbfc0c565a26eac3a712bfe71de
MN Raza <mnraza94@gmail.com>
2024-12-29 04:57:29 +0530

    Login  screen

--- EmployeeManagementSystem/src/components/Auth/Login.jsx
+++ EmployeeManagementSystem/src/components/Auth/Login.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
 const Login = () => {
+
+  const submitHandler = () => {
+    console.log("Hello guys!, Form submitted");
+
+  }
   return (
     <div className='h-screen w-screen  flex items-center justify-center'>
 
-      <div className='w-[400px] h-auto p-4 rounded-lg drop-shadow-md border-2 border-gray-700 shadow-lg'>
-        <form className='flex flex-col gap-4 justify-center items-center'>
+      <div className='w-[400px] h-auto p-6 rounded-2xl drop-shadow-md border-2 border-emerald-800 shadow-lg'>
+        <form onSubmit={submitHandler} className='flex flex-col gap-4 justify-center items-center'>
           <h1 className='text-2xl'>Login</h1>
-          <hr className='m-2 w-full border-1  border-gray-700 ' />
-          <input className='p-2 w-full rounded-md
+          <input className='placeholder:text-gray-400  border-2 bg-transparent outline-none border-emerald-800  px-2 py-4 w-full rounded-xl
           ' type="text" name="email" placeholder='Enter your email' id="" />
-          <input className='p-2 w-full rounded-md' type="password" name="password" placeholder='Enter your password' id="" />
-          <hr className='mt-4 w-full border-1  border-gray-700 ' />
+          <input className='placeholder:text-gray-400   bg-transparent outline-none border-emerald-800 px-2 py-4  w-full rounded-xl border-2' type="password" name="password" placeholder='Enter your password' id="" />
 
-          <button className='w-full bg-slate-950 hover:bg-slate-900 text-white p-2 rounded-md' type="button">Login</button>
+          <button className='w-full bg-emerald-800 hover:bg-emerald-900 text-white p-2 rounded-md' type="submit">Login</button>
 
         </form>
 
