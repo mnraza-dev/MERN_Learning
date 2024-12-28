d5a6b0d02e87d33cb51a65b5f50cdc40b9d08613
MN Raza <mnraza94@gmail.com>
2024-12-28 06:58:54 +0530

    filter and map

--- refresher/script.js
+++ refresher/script.js
@@ -1,10 +1,19 @@
 // maps
 const names = ["John", "Paul", "George", "Ringo"];
-const newNames = names.map(name=>{
+const newNames = names.map(name => {
     return name.toUpperCase() + " G";
 })
 
 console.log(newNames);
 
 // filter
+const namesFilter = ["John", "Paul", "George", "Ringo"];
+
+const newNamesFilter = namesFilter.filter(name => {
+    return name.length > 4;
+})
+
+console.log(newNamesFilter);
+
+
 
