85d086e9c5b10957d3c19064d521da8464c94a8d
MN Raza <mnraza94@gmail.com>
2024-12-29 02:38:16 +0530

    used useState() ook

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,19 @@
 
+import { useState } from 'react'
 import './App.css'
 function App() {
 
+  const [name, setName] = useState("Jane B");
+
+  const changeName = () => {
+    setName("John Doe");
+  }
+
+
   return (
     <>
-     <h1>Hello there</h1>
+      <h1>Hello {name}!</h1>
+      <button onClick={changeName }>Change User</button>
     </>
   )
 }
