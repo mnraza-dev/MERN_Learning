10e7a46c748a1c6951077d0d0f0dea6f12d7ccda
MN Raza <mnraza94@gmail.com>
2024-12-28 07:27:12 +0530

    refresher for reactJS

--- refresher/script.js
+++ refresher/script.js
@@ -1,7 +1,10 @@
 // maps
 const names = ["John", "Paul", "George", "Ringo"];
+
 const newNames = names.map(name => {
+
     return name.toUpperCase() + " G";
+
 })
 
 console.log(newNames);
@@ -10,7 +13,9 @@ console.log(newNames);
 const namesFilter = ["John", "Johny", "Paul", "George", "Ringo", "Jenee", "Jeneffer", "Jane"];
 
 const newNamesFilter = namesFilter.filter(name => {
+
     return name.startsWith("J");
+    
 })
 
 console.log(newNamesFilter);
@@ -36,7 +41,9 @@ console.log(zip);
 // Spread & Rest (...)
 
 const namesSpread = ["John", "Paul", "George", "Ringo"];
+
 const copyNames = namesSpread; //this way references the same array, so to copy the array we need to use the spread operator
+
 console.log(namesSpread, copyNames);
 
 const newNamesSpread = [...namesSpread, "Jenee", "Jeneffer", "Jane"];
@@ -51,25 +58,23 @@ const car = {
     year: 2009
 }
 const newCar = { ...car, color: "red" }
+
 console.log(newCar);
 
 console.log(car);
 
-
 // Rest Operator (...)
 
 const numbers = [1, 12, 3, 14, 5];
+
 const [one, two, ...rest] = numbers;
 
 console.log(one, two, rest);
 
 function restNumbers(a,b,...numbers) {
+
     console.log(a, b, numbers);
     
 }
+
 restNumbers(1, 2, 3, 4, 5);
-
-
-
-
-
