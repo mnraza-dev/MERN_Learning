72b4ccd274059d0d78529312aee1a6bb498e2ad4
MN Raza <mnraza94@gmail.com>
2024-12-29 02:53:28 +0530

    Used two way binding

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -8,12 +8,28 @@ function App() {
   const changeName = () => {
     setName("John Doe");
   }
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+  }
 
 
   return (
     <>
       <h1>Hello {name}!</h1>
-      <button onClick={changeName }>Change User</button>
+      {/* <button onClick={changeName}>Change User</button> */}
+
+      <form onSubmit={submitHandler}>
+
+        <input type="text" name="name" id="" value={name} onChange={(e) => setName(e.target.value)} />
+
+        <button type='submit'>
+          Change User
+        </button>
+
+      </form>
+
+
     </>
   )
 }
